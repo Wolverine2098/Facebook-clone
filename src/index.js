988ed987874e4918e227a8f5063721cf7cd37ebf
Raunak <raunak.goswami@indiamart.com>
2022-06-30 22:26:34 +0530

    setting up facebook clone project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './components';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,5 +13,8 @@ root.render(
 
   
 );
+
+
+
 
 
