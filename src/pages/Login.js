7642f9630f25dede43a36b81bb194998967ee238
Raunak <raunak.goswami@indiamart.com>
2022-07-06 01:43:00 +0530

    adding api calls to the function

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,60 @@
+import { useState } from 'react';
 import styles from '../styles/login.module.css';
+import { useToasts } from 'react-toast-notifications';
+import { login } from '../api';
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState('');
+  const { addToast } = useToasts();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoggingIn(true);
+    if (!email || !password) {
+      return addToast('please enter both email and password', {
+        appearance: 'error',
+      });
+    }
+    const response = await login(email, password);
+    if (response.success) {
+      addToast('Successfully logged in', {
+        appearance: 'success',
+      });
+    } else
+      addToast(response.message, {
+        appearance: 'error',
+      });
+
+    setLoggingIn(false);
+  };
   return (
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <span className={styles.loginSignupHeader}>log in</span>
       <div className={styles.field}>
-        <input type="email" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
       </div>
       <div className={styles.field}>
-        <input type="password" placeholder="Password" required />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
       <div className={styles.field}>
-        <button>log in</button>
+        <button disabled={loggingIn}>
+          {loggingIn ? 'Logging in..' : 'log In'}
+        </button>
       </div>
     </form>
   );
