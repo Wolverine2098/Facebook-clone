dd555574a0fbb20b0d9eb5e6653082aff7141479
Raunak <raunak.goswami@indiamart.com>
2022-07-06 01:02:38 +0530

    Developing the login UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,19 @@
+import styles from '../styles/login.module.css';
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <form className={styles.loginForm}>
+      <span className={styles.loginSignupHeader}>log in</span>
+      <div className={styles.field}>
+        <input type="email" placeholder="Email" required />
+      </div>
+      <div className={styles.field}>
+        <input type="password" placeholder="Password" required />
+      </div>
+      <div className={styles.field}>
+        <button>log in</button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
