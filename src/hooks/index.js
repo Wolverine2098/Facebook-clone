138e472b191b0bce42c5593cd6e4b2016a9fd994
Raunak <raunak.goswami@indiamart.com>
2022-07-07 01:12:12 +0530

    Adding the authorisation to the code

---
+++ src/hooks/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export const useProvideAuth = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const login = (email, password) => {};
+
+  const logout = () => {};
+
+  return {
+    user,
+    login,
+    logout,
+    loading,
+  };
+};
